52d5168ebdd2137ab5978c9015ddf237ee4d86af
chen-jianfu <xmjianfu@163.com>
2017-05-03 21:57:01 +0800

    人脸检测更新

--- Source/facedetectUnit.pas
+++ Source/facedetectUnit.pas
@@ -1,16 +1,17 @@
 unit facedetectUnit;
 
 interface
-function facedetect_frontal_extern (imagepath:PAnsiChar;scale:Single;min_neighbors:Integer;min_size,max_size:Integer):PInteger; stdcall; external'facedetect.dll'
-function facedetect_frontal(imagepath:PAnsiChar;scale:Single;min_neighbors:Integer;min_size,max_size:Integer):Integer;
+function facedetect_frontal_extern (imagepath:PChar;scale:Single;min_neighbors:Integer;min_size,max_size:Integer):PInteger; stdcall; external'facedetect.dll'
+function facedetect_multiview_reinforce_extern (imagepath:PChar;scale:Single;min_neighbors:Integer;min_size,max_size:Integer):PInteger; stdcall; external'facedetect.dll'
+function facedetect_frontal(imagepath:PChar;scale:Single;min_neighbors:Integer;min_size,max_size:Integer):Integer;
 
 implementation
 
-function facedetect_frontal(imagepath:PAnsiChar;scale:Single;min_neighbors:Integer;min_size,max_size:Integer):Integer;
+function facedetect_frontal(imagepath:PChar;scale:Single;min_neighbors:Integer;min_size,max_size:Integer):Integer;
 var
   P:PInteger;
 begin
-   P:=  facedetect_frontal_extern (imagepath, scale, min_neighbors, min_size, max_size) ;
+   P:=  facedetect_multiview_reinforce_extern (imagepath, scale, min_neighbors, min_size, max_size) ;
   Result:=(P^) ;
 end;   
 
